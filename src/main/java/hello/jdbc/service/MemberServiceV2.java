1f1fcffdafaee7708442683f85ce6c1e6020ebc5
Bsh <dnfg12@naver.com>
2022-06-20 18:45:47 +0900

    Initial commit

---
+++ src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,59 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+@Slf4j
+public class MemberServiceV2 {
+
+    private final MemberRepositoryV2 memberRepositoryV2;
+    private final DataSource dataSource;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Connection con = dataSource.getConnection();
+        try{
+            con.setAutoCommit(false);   //트랜잭션 시작
+            //비즈니스 로직
+            bizLogic(con, fromId, toId, money);
+            con.commit();  //성공시 커밋
+        } catch (Exception e) {
+            con.rollback();  // 실패시 롤백
+            throw new IllegalStateException(e);
+        } finally {
+            release(con);
+        }
+    }
+
+    private void bizLogic(Connection con, String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepositoryV2.findById(con, fromId);
+        Member toMember = memberRepositoryV2.findById(con, toId);
+
+        memberRepositoryV2.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepositoryV2.update(toId, toMember.getMoney() + money);
+    }
+
+    private void release(Connection con) {
+        if(con != null) {
+            try{
+                con.setAutoCommit(true);  //커넥션 풀 고려
+                con.close();
+            } catch (Exception e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
